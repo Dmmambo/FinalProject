171ed1d50dfa63b052629d3196b6ea5787d6a8bd
Hope Kalitera <hopekalitera@gmail.com>
2024-10-07 14:15:26 +0200

    chore: process applicant data

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
+// routes.js
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+// Define the upload directory
+const uploadDir = path.join(__dirname, '..', 'images', 'applicants');
+
+// Ensure the upload directory exists
+fs.mkdirSync(uploadDir, { recursive: true });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
 
 const authController = require('../controllers/authController'); // Update this to match the correct file
+const applicantController = require('../controllers/applicant'); // Update this to match the correct file
 
 // middlewares/auth.js
 // function ensureAuthenticated(req, res, next) {
@@ -25,9 +48,19 @@ function (req, res, next) {
 router.get('/', authController.home);
 router.get('/login', authController.login);
 router.get('/verifyTOTP', authController.verifyGet);
-router.get('/application',  authController.application);
 router.get('/policeDashboard',  authController.policeDashboard);
 router.get('/logout', authController.logoutget);
+
+//  application routes
+
+router.get('/application',  authController.application);
+// router.post('/application/create',  applicantController.application);
+router.post('/add-participant', upload.fields([
+    { name: 'passportPhoto', maxCount: 1 },
+    { name: 'nationalID', maxCount: 1 },
+    { name: 'fingerprintdata', maxCount: 1 },
+    { name: 'paymentProof', maxCount: 1 }
+]), applicantController.addParticipant);
 
 router.get('/manageApplication', authController.manageApplication);
 router.get('/generateCertificate', authController.generateCertificate);
